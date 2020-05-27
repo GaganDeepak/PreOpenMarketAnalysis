9600aaa4bc22319353f9b08d3ec1f1416ec0a41a
Gagan <gagan.databee@gmail.com>
2020-05-27 23:11:57 +0530

    rounding decimal number to 2 digits

--- Analysis.py
+++ Analysis.py
@@ -5,12 +5,12 @@ import pandas as pd
 df = pd.read_csv("pom.csv")
 
 for ind, row in df.iterrows():
-    df.loc[ind,'2daychng%'] = ((row['Close']-row['prevClose'])/row['prevClose'])*100
-    df.loc[ind,'2dayhigh%'] = ((row['High']-row['open_price'])/row['open_price'])*100
-    df.loc[ind,'2daylow%'] = ((row['open_price']-row['Low'])/row['open_price'])*100
+    df.loc[ind,'2daychng%'] = round(((row['Close']-row['prevClose'])/row['prevClose'])*100,2)
+    df.loc[ind,'2dayhigh%'] = round(((row['High']-row['open_price'])/row['open_price'])*100,2)
+    df.loc[ind,'2daylow%'] = round(((row['open_price']-row['Low'])/row['open_price'])*100,2)
 
 for ind, row in df.iterrows():
-    df.loc[ind,'swing%'] = (row['2dayhigh%']-row['2daylow%'])
+    df.loc[ind,'swing%'] = round((row['2dayhigh%']-row['2daylow%']),2)
 
 
 for ind, row in df.iterrows():
